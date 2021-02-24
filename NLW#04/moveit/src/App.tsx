9d7265a4d84e350975b5f24bd4e69231f05feb57
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-02-23 21:53:32 -0300

    Primeiro dia, rumo ao próximo nível

---
+++ NLW#04/moveit/src/App.tsx
@@ -0,0 +1,14 @@
+
+import { ExperienceBar } from './componets/ExperienceBar';
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="container" >  
+      <ExperienceBar/>
+    </div>
+   
+  );
+}
+
+export default App;
